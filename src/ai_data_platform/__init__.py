69facf1571dc3e5295bee300cea2143e25ee6c5e
Preetham Somsole <preetham.somsole@gmail.com>
2026-09-14 14:31:16 -0700

    feat: expose complete public capability layer

--- src/ai_data_platform/__init__.py
+++ src/ai_data_platform/__init__.py
@@ -1,7 +1,9 @@
 """Vendor-neutral semantic registry and deterministic dataset discovery."""
 
 from ai_data_platform.api import (
+    assess_ambiguity,
     discover_datasets,
+    get_concept,
     get_dataset,
     get_entity,
     get_metric,
@@ -11,7 +13,9 @@ from ai_data_platform.api import (
 from ai_data_platform.registry.loader import load_registry
 
 __all__ = [
+    "assess_ambiguity",
     "discover_datasets",
+    "get_concept",
     "get_dataset",
     "get_entity",
     "get_metric",
