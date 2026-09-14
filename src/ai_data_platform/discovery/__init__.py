0853eb65e572fcf566e4171a1687ad33cc60f69d
Preetham Somsole <preetham.somsole@gmail.com>
2026-09-14 14:30:43 -0700

    feat: expose discovery diagnostics

--- src/ai_data_platform/discovery/__init__.py
+++ src/ai_data_platform/discovery/__init__.py
@@ -1,14 +1,18 @@
 from ai_data_platform.discovery.service import (
+    DatasetExclusion,
     DiscoveryResult,
     DiscoveryStatus,
+    MetricMatch,
     assess_ambiguity,
     discover_datasets,
     resolve_metric,
 )
 
 __all__ = [
+    "DatasetExclusion",
     "DiscoveryResult",
     "DiscoveryStatus",
+    "MetricMatch",
     "assess_ambiguity",
     "discover_datasets",
     "resolve_metric",
